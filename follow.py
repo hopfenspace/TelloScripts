96333c901a152268db839c63c1f07bacc546f624
Jakob Löw <jakob@m4gnus.de>
2019-04-09 10:43:40 +0200

    calculate forward/back, up/down and rotation speed aiming at always centering the target in the middle of the camera

--- follow.py
+++ follow.py
@@ -1,8 +1,12 @@
 from djitellopy import Tello
 import cv2
+import math
+
+speed_factor = 400
 
 face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
 tracker = None
+startSize = 0
 lastSuccessfullTrack = 0
 
 clickPos = None
@@ -35,6 +39,29 @@ while True:
 		ok, bbox = tracker.update(img)
 		if ok:
 			x, y, w, h = bbox
+			centerX = x + w / 2
+			centerY = y + h / 2
+			size = w * h
+			width, height, channel = img.shape
+
+			normDistance = (startSize - size) / (width * height)
+			normHeight = (centerY - height / 2) / height
+			normAngle = (centerX - width / 2) / width
+
+			forwardSpeed = speed_factor * (normDistance * normDistance * normDistance)
+			heightSpeed = speed_factor * (normHeight * normHeight * normHeight)
+			rotationSpeed = speed_factor * (normAngle * normAngle * normAngle)
+
+			if math.fabs(forwardSpeed) > 100:
+				forwardSpeed = math.copysign(100, forwardSpeed)
+			if math.fabs(heightSpeed) > 100:
+				heightSpeed = math.copysign(100, heightSpeed)
+			if math.fabs(rotationSpeed) > 100:
+				rotationSpeed = math.copysign(100, rotationSpeed)
+
+			print(normDistance, normHeight, normAngle, forwardSpeed, heightSpeed, rotationSpeed)
+			#tello.send_rc_control(0, forwardSpeed, heightSpeed, rotationSpeed)
+
 			x, y, w, h = int(x), int(y), int(w), int(h)
 			cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
 		else:
@@ -43,7 +70,7 @@ while True:
 				print("Tracker did not find anything for 60 frames, aborting...")
 				tracker = None
 
-		
+
 
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -65,6 +92,7 @@ while True:
 			if clickX >= x and clickX <= x + w and clickY >= y and clickY <= y + h:
 				tracker = cv2.TrackerMedianFlow_create()
 				if tracker.init(img, (x, y, w, h)):
+					startSize = w * h
 					print("Starting to track object!")
 				else:
 					print("Failed initializing the tracker!")
